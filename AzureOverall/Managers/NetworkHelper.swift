8331617e4b9ed207958599d74b448ac51e6f0aa6
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-07-31 17:47:07 -0400

    Adds NetworkError file

---
+++ AzureOverall/Managers/NetworkHelper.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkHelper.swift
+//  AzureOverall
+//
+//  Created by Jocelyn Boyd on 7/31/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import Foundation
---
+++ AzureOverall/Utilities/NetworkError.swift
@@ -0,0 +1,28 @@
+//
+//  AOError.swift
+//  AzureOverall
+//
+//  Created by Jocelyn Boyd on 7/31/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import Foundation
+
+// the enum conforms to the Error protocol and lists some of the potential things that can go wrong when getting data
+
+enum NetworkError: Error {
+  case unauthenticated
+  case invalidResponse
+  case couldNotParseJSON(rawError: Error)
+  case noInternetConnection
+  case badURL
+  case badStatusCode
+  case noDataReceived
+  case notAnImage
+  case other(rawError: Error)
+  case couldNotEncode
+  case responseError
+  case unableToComplete
+  case invalidData
+  case badURLResponse
+}

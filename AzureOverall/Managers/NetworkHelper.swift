e021aad568346e5409e87cea2a810f08b8f918fc
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-07-31 17:48:10 -0400

    Adds NetworkHelper file

--- AzureOverall/Managers/NetworkHelper.swift
+++ AzureOverall/Managers/NetworkHelper.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+class NetworkHelper {
+  // MARK: - Static Properties
+  
+  // because there is only one instance (named manager), there's a guarantee that anywhere in the app that reads from or writes to the NetworkHelper is looking at the same place
+  static let manager = NetworkHelper() // type property
+  
+  // MARK: - Internal Methods
+  
+  func getData(from urlString: String, completionHandler: @escaping (Result<Data, NetworkError>) -> Void) {
+    
+    guard let url = URL(string: urlString) else {
+      completionHandler(.failure(.badURL))
+      return
+    }
+    
+    let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+      
+      if let _ = error {
+        completionHandler(.failure(.unableToComplete))
+        return
+      }
+      
+      guard let urlResponse = response as? HTTPURLResponse else {
+        completionHandler(.failure(.invalidResponse))
+        return
+      }
+      
+      guard let data = data else {
+        completionHandler(.failure(.invalidData))
+        return
+      }
+      
+      switch urlResponse.statusCode {
+      case 200...299: break
+      default:
+        completionHandler(.failure(.badURLResponse))
+        return
+      }
+      completionHandler(.success(data))
+    }
+    // make the call to get data from the URL by using the data task's reume()
+    dataTask.resume()
+  }
+  // MARK: - Private Properties and Initializers
+  
+  private let urlSession = URLSession(configuration: .default)
+  
+  // mark the initializer as private to only create NetworkHelper instances inside of our class
+  private init() {}
+  
+}

afaa8d6239bbdf5fd62480716bb729c23b5c42e8
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-08-01 00:01:44 -0400

    Adds ImageHelper file

---
+++ AzureOverall/Managers/ImageHelper.swift
@@ -0,0 +1,35 @@
+//
+//  ImageHelper.swift
+//  AzureOverall
+//
+//  Created by Jocelyn Boyd on 7/31/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class ImageHelper {
+  
+  // MARK: - Static Properties
+  static let manager = ImageHelper()
+  
+  // MARK: - Internal Properties
+  func getImage(from urlString: String, completionHandler: @escaping (Result<UIImage, NetworkError>) -> Void) {
+    
+    NetworkHelper.manager.getData(from: urlString) { (result) in
+      switch result {
+      case let .failure(error):
+        completionHandler(.failure(error))
+      case let .success(data):
+        guard let onlineImage = UIImage(data: data) else {
+          completionHandler(.failure(.notAnImage))
+          return
+        }
+        completionHandler(.success(onlineImage))
+      }
+    }
+  }
+  
+  // MARK: - Private Properties and Initializers 
+  private init() {}
+}

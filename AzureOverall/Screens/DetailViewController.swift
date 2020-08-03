b8245126bcdc70bcd6b6807f6db633f872356561
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-08-03 18:40:51 -0400

    Adds recipe DetailViewController

--- AzureOverall/Screens/DetailViewController.swift
+++ AzureOverall/Screens/DetailViewController.swift
@@ -10,8 +10,47 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+  var recipeDetails: Recipe?
+  
+  lazy var recipeImageView: UIImageView = {
+    let image = UIImageView()
+    return image
+  }()
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+      loadRecipeDetails()
+      configureUI()
     }
 
+  
+  private func loadRecipeDetails() {
+    guard let recipeDetails = recipeDetails else { return }
+    ImageFetchingService.manager.getImage(from: recipeDetails.id) { [weak self] (result) in
+      guard let self = self else { return }
+      DispatchQueue.main.async {
+        switch result {
+        case let .success(image):
+          self.recipeImageView.image = image
+        case let .failure(error):
+          print(error)
+        }
+      }
+    }
+  }
+  
+  
+  private func configureUI() {
+    view.addSubview(recipeImageView)
+    recipeImageView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      recipeImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      recipeImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      recipeImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      recipeImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+    ])
+  }
+  
+  
 }

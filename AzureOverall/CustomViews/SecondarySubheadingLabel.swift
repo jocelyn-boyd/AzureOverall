c01bb98762d8d71b53630bbc05451e1bca08c105
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-08-03 21:29:36 -0400

    Adds custom SecondaryTitle label

---
+++ AzureOverall/CustomViews/SecondarySubheadingLabel.swift
@@ -0,0 +1,38 @@
+//
+//  SecondarySubheadingLabel.swift
+//  AzureOverall
+//
+//  Created by Jocelyn Boyd on 8/3/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class SecondaryLabel: UILabel {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  convenience init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+    self.init(frame: .zero)
+    self.textAlignment = textAlignment
+    self.font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+  }
+  
+  
+  private func configure() {
+    textColor = .secondaryLabel
+    numberOfLines = 0
+    lineBreakMode = .byWordWrapping
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+}

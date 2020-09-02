645f2c32d3e0b3660301be216e5f202fead32f7e
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-09-02 00:28:48 -0400

    Cleans up code in DetailVC

--- AzureOverall/Screens/DetailViewController.swift
+++ AzureOverall/Screens/DetailViewController.swift
@@ -7,66 +7,80 @@ import UIKit
 
 class DetailViewController: UIViewController {
   
-  // MARK: Properties
   var recipe: Recipe?
   var recipeInformation = [RecipeInformation]()
   
-  let recipeImageView = AOFoodImageView()
+  // MARK: UI Properties
   let recipeTitleLabel = AOTitleLabel(textAlignment: .center, fontSize: 20)
-  let veganLabel = AOSubheadingLabel(textAlignment: .center, fontSize: 18)
-  let vegetarianLabel = AOSubheadingLabel(textAlignment: .center, fontSize: 18)
-  let glutenFreeLabel = AOSubheadingLabel(textAlignment: .center, fontSize: 18)
-  let dairyFreeLabel = AOSubheadingLabel(textAlignment: .center, fontSize: 18)
+  let recipeImageView = AOFoodImageView()
   
+  let veganLabel = AOBodyLabel(textAlignment: .left, fontSize: 18)
+  let vegetarianLabel = AOBodyLabel(textAlignment: .left, fontSize: 18)
+  let glutenFreeLabel = AOBodyLabel(textAlignment: .left, fontSize: 18)
+  let dairyFreeLabel = AOBodyLabel(textAlignment: .left, fontSize: 18)
   
-  // MARK: Lifecycle Methods
+  let sourceNameLabel = AOBodyLabel(textAlignment: .right, fontSize: 20)
+  let sourceURLButton = AOButton(backgroundColor: Constants.AppColorPalette.uaRed, title: "Go To Instructions")
+  
+  // MARK: - Private Properties
+  private let padding: CGFloat = 20
+  
+  // MARK: - Lifecycle Methods
   override func viewDidLoad() {
     super.viewDidLoad()
+    addSubviews()
     configureViewController()
-    configureLayoutUI()
+    configureUIElements()
+    configureRecipeTitleLabel()
+    configureRecipeImageView()
+    configureDietaryStackViewOne()
+    configureDietaryStackViewTwo()
+    configureSourceUIElements()
     loadSingleRecipeDetails()
   }
   
+  // MARK: - Private Methods
+  private func loadRecipeDetails(from recipeInfo: RecipeInformation) {
+    recipeTitleLabel.text = recipeInfo.title
+    sourceNameLabel.text = "Source: \(recipeInfo.sourceName ?? "")"
+    recipeImageView.downloadImage(fromURL: recipeInfo.id)
+  }
   
-  // MARK: Private Methods
+  private func loadDietaryDetails(from dietInfo: RecipeInformation) {
+    if dietInfo.vegan {
+      veganLabel.text = "✅ Vegan"
+    } else {
+      veganLabel.text = "❎ Vegan"
+    }
+    
+    if dietInfo.vegetarian {
+      vegetarianLabel.text = "✅ Vegetarian"
+    } else {
+      vegetarianLabel.text = "❎ Vegetarian"
+    }
+    
+    if dietInfo.glutenFree {
+      glutenFreeLabel.text = "✅ Gluten Free"
+    } else {
+      glutenFreeLabel.text = "❎ Gluten Free"
+    }
+    
+    if dietInfo.dairyFree {
+      dairyFreeLabel.text = "✅ Dairy Free"
+    } else {
+      dairyFreeLabel.text = "❎ Dairy Free"
+    }
+  }
+  
   private func loadSingleRecipeDetails() {
     guard let recipe = recipe else { return }
-    recipeTitleLabel.text = recipe.title
-    recipeTitleLabel.numberOfLines = 0
-    
-    recipeImageView.downloadImage(fromURL: recipe.id)
-    
     RecipeFetchingService.manager.fetchSingleRecipe(from: recipe.id) { [weak self] (result) in
       guard let self = self else { return }
       DispatchQueue.main.async {
         switch result {
         case let .success(data):
-          
-          // TODO: Refactor this code
-          if data.vegan == false {
-            self.veganLabel.text = "Not Vegan"
-          } else {
-            self.veganLabel.text = "Is Vegan"
-          }
-          
-          if data.vegetarian == false {
-            self.vegetarianLabel.text = "Not Vegetarian"
-          } else {
-            self.vegetarianLabel.text = "Is Vegetarian"
-          }
-          
-          if data.glutenFree == false {
-            self.glutenFreeLabel.text = "Not Gluten Free"
-          } else {
-            self.glutenFreeLabel.text = "Is Gluten Free"
-          }
-          
-          if data.dairyFree == false {
-            self.dairyFreeLabel.text = "Not Dairy Free"
-          } else {
-            self.dairyFreeLabel.text = "Is Dairy Free"
-          }
-          
+          self.loadRecipeDetails(from: data)
+          self.loadDietaryDetails(from: data)
         case let .failure(error):
           print(error)
         }
@@ -74,60 +88,84 @@ class DetailViewController: UIViewController {
     }
   }
   
+  private func addSubviews() {
+    let itemViews = [recipeImageView, recipeTitleLabel, veganLabel, vegetarianLabel, glutenFreeLabel, dairyFreeLabel, sourceNameLabel, sourceURLButton]
+    for itemView in itemViews { view.addSubview(itemView) }
+  }
   
-  // MARK: Configuration Methods
+  // MARK: - Configuration Methods
   private func configureViewController() {
     view.backgroundColor = .systemBackground
     let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissDetailVC))
     navigationItem.rightBarButtonItem = doneButton
   }
   
+  private func configureUIElements() {
+    recipeTitleLabel.numberOfLines = 0
+    sourceNameLabel.textColor = Constants.AppColorPalette.richBlackFOGRA39
+  }
   
-  private func configureLayoutUI() {
-    let padding: CGFloat = 20
-    
-    let itemViews = [recipeImageView, recipeTitleLabel, veganLabel, vegetarianLabel, glutenFreeLabel, dairyFreeLabel]
-    for itemView in itemViews { view.addSubview(itemView) }
-    
-    let itemLabels = [veganLabel, vegetarianLabel, glutenFreeLabel, dairyFreeLabel]
-    for labels in itemLabels {
-      labels.layer.borderColor = Constants.AppColorPalette.richBlackFOGRA39.cgColor
-      labels.layer.borderWidth = 1
-      labels.layer.cornerRadius = 10
-    }
-    
-    
+  func configureRecipeTitleLabel() {
     NSLayoutConstraint.activate([
       recipeTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
       recipeTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
       recipeTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
-      recipeTitleLabel.bottomAnchor.constraint(equalTo: recipeImageView.topAnchor, constant: -padding),
-      
-      recipeImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-      recipeImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
-      recipeImageView.heightAnchor.constraint(equalToConstant: 200),
-      
-      veganLabel.topAnchor.constraint(equalTo: recipeImageView.bottomAnchor, constant: padding),
-      veganLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-      veganLabel.widthAnchor.constraint(equalToConstant: view.bounds.width / 2.3),
-      
-      glutenFreeLabel.topAnchor.constraint(equalTo: recipeImageView.bottomAnchor, constant: padding),
-      glutenFreeLabel.leadingAnchor.constraint(equalTo: veganLabel.trailingAnchor),
-      glutenFreeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
-      
-      vegetarianLabel.topAnchor.constraint(equalTo: veganLabel.bottomAnchor, constant: padding / 3),
-      vegetarianLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-      vegetarianLabel.widthAnchor.constraint(equalToConstant: view.bounds.width / 2.3),
-      
-      dairyFreeLabel.topAnchor.constraint(equalTo: glutenFreeLabel.bottomAnchor, constant: padding / 3),
-      dairyFreeLabel.leadingAnchor.constraint(equalTo: vegetarianLabel.trailingAnchor),
-      dairyFreeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+      recipeTitleLabel.bottomAnchor.constraint(equalTo: recipeImageView.topAnchor, constant: -padding)
     ])
   }
   
-  
-  // MARK: @objc Methods
-  @objc func dismissDetailVC() {
-    dismiss(animated: true)
+  func configureRecipeImageView() {
+    NSLayoutConstraint.activate([
+      recipeImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+      recipeImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+      recipeImageView.heightAnchor.constraint(equalToConstant: 200)
+    ])
   }
+  
+  func configureSourceUIElements() {
+    NSLayoutConstraint.activate([
+      sourceNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      sourceNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+      sourceNameLabel.bottomAnchor.constraint(equalTo: sourceURLButton.topAnchor, constant: -padding),
+      
+      sourceURLButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+      sourceURLButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      sourceURLButton.widthAnchor.constraint(equalToConstant: 250)
+    ])
+  }
+  
+  func configureDietaryStackViewOne() {
+    let stackView = UIStackView(arrangedSubviews: [veganLabel, vegetarianLabel])
+    stackView.axis = .horizontal
+    stackView.spacing = 50
+    stackView.distribution = .fillEqually
+    view.addSubview(stackView)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      stackView.topAnchor.constraint(equalTo: recipeImageView.bottomAnchor, constant: padding),
+      stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+      stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+    ])
+  }
+  
+  func configureDietaryStackViewTwo() {
+    let stackView = UIStackView(arrangedSubviews: [glutenFreeLabel, dairyFreeLabel])
+    stackView.axis = .horizontal
+    stackView.spacing = 50
+    stackView.distribution = .fillEqually
+    view.addSubview(stackView)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      stackView.topAnchor.constraint(equalTo: veganLabel.bottomAnchor, constant: padding / 2),
+      stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+      stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+    ])
+  }
+  
+  // MARK: - @objc Methods
+   @objc func dismissDetailVC() {
+     dismiss(animated: true)
+   }
 }
--- AzureOverall/Support/SceneDelegate.swift
+++ AzureOverall/Support/SceneDelegate.swift
@@ -19,8 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     window = UIWindow(frame: windowScene.coordinateSpace.bounds)
     window?.windowScene = windowScene
-//    window?.rootViewController = AOTabBarController()
-    window?.rootViewController = HomeAuthViewController()
+    window?.rootViewController = AOTabBarController()
+//    window?.rootViewController = HomeAuthViewController()
     window?.makeKeyAndVisible()
   }
 

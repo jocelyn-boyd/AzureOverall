6685410b630d7c428d57a76712e46ee96abcc2d0
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-09-02 00:31:59 -0400

    Adds UIViewControlller+Ext

---
+++ AzureOverall/Utilities/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  AzureOverall
+//
+//  Created by Jocelyn Boyd on 9/2/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import Foundation
+import SafariServices
+
+extension UIViewController {
+  func presentSafairVC(with url: URL) {
+    let safariVC = SFSafariViewController(url: url)
+    safariVC.preferredBarTintColor = Constants.AppColorPalette.uaRed
+    present(safariVC, animated: true)
+  }
+  
+  
+}
